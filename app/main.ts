d4ac5100906b526e39def7217d3aa393f7dbab5e
Ihor Pavliuk <akira.pavl@gmail.com>
2025-04-01 13:15:33 +0300

    added test console log

--- app/main.ts
+++ app/main.ts
@@ -3,12 +3,12 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
   socket.on("connect", () => {
+    console.log("we're here");
     socket.write("HTTP/1.1 200 OK\r\n\r\n");
   });
 });
