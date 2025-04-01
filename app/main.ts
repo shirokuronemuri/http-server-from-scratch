6944530e1d8151e7574511b4ced5854bad5f8757
Ihor Pavliuk <akira.pavl@gmail.com>
2025-04-01 15:12:13 +0300

    implemented simple request handling

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,20 @@
-import * as net from "net";
+import * as net from "node:net";
 
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (request) => {
+    const RequestData = request.toString().split('\r\n');
+    const [method, target] = RequestData[0].split(' ');
+    if (method === 'GET') {
+      switch (target) {
+        case '/': {
+          socket.write("HTTP/1.1 200 OK\r\n\r\n");
+          break;
+        }
+        default: {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+      }
+    }
   });
 
   socket.on("close", () => {
